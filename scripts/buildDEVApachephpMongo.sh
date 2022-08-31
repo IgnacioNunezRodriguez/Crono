76c41dc0e0df8832c6f486232385b56ceb2bcb7e
Ignacio Núñez <inr_jw@hotmail.com>
2022-08-31 13:30:28 +0200

    Reorganización de scripts

--- scripts/buildDEVApachephpMongo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-composer install --optimize-autoloader --dev --working-dir=./src
-docker-compose  --env-file ./env/.dev.env -f ./docker/docker-compose.build.dev.yml up --build -d apachephp mongo
--- scripts/deployPROApachephpMysql.example.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker-compose --env-file env/.pro.example.env -f docker/docker-compose.pull.gitlab.pro.yml up --build -d apachephp mysql
