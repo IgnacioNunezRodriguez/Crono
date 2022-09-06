259f6c3458a5320d9cd427e47b26bcb509b7b548
Ignacio Núñez <inr_jw@hotmail.com>
2022-09-06 18:36:20 +0200

    Reorganización de scripts

--- scripts/docker/deployPROApachephpMysqlMongo.example.sh
+++ scripts/docker/deployPROApachephpMysqlMongo.example.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose --env-file ../env/.pro.example.env -f ../docker/docker-compose.pull.pro.yml up --build -d
+docker-compose --env-file ./env/.pro.example.env -f ./docker/docker-compose.pull.pro.yml up --build -d
