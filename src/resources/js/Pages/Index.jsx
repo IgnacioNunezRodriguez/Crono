a9f91140b28a53843194da192a4663e527e6f312
Ignacio Núñez <inr_jw@hotmail.com>
2022-09-09 13:32:09 +0200

    Rediseño ahora es más claro

--- src/resources/js/Pages/Index.jsx
+++ src/resources/js/Pages/Index.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
 import { Link, Head } from '@inertiajs/inertia-react';
 import Chronometer from '@/Components/Chronometer.jsx';
+import Logo from "@/Components/ApplicationLogo";
+
+
+
 
 export default function Index(props) {
     return (
         <>
             <Head title="Crono" />
-            <div id="wrapper" className="flex flex-col h-screen w-screen">
-                <div id="rightBar" className="w-screen basis-8">
-                    {props.auth.user ? (
-                        <Link href={route('dashboard')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <>
-                            <Link href={route('login')} className="text-sm text-gray-700 dark:text-gray-500 underline">
-                                Log in
+            <div id="wrapper" className="flex flex-col justify-center h-screen w-screen">
+                <div id="topbar" className="flex flex-row justify-end p-4">
+                    <div id="rightBar" className="">
+                        {props.auth.user ? (
+                            <Link href={route('dashboard')} className="rounded-full text-sm text-gray-50 dark:text-gray-50 bg-slate-500 px-4 py-2">
+                                Dashboard
                             </Link>
+                        ) : (
+                            <>
+                                <Link href={route('login')} className="rounded-full text-sm text-gray-50 dark:text-gray-50 bg-slate-500 px-4 py-2">
+                                    Log in
+                                </Link>
 
-                            <Link href={route('register')} className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
-                                Register
-                            </Link>
-                        </>
-                    )}
+                                <Link href={route('register')} className="rounded-full ml-4 text-sm text-gray-50 dark:text-gray-500 bg-slate-500 px-4 py-2">
+                                    Register
+                                </Link>
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div id="logo" className="flex flex-row justify-center mx-8 bg-red-600">
+                    <Logo />
                 </div>
 
                 <div id="main" className="grow w-screen">
