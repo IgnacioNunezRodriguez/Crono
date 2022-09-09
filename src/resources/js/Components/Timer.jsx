95e4923535abdeca7bb652125dd22025d70b4702
Ignacio Núñez <inr_jw@hotmail.com>
2022-09-09 13:31:02 +0200

    No volverá a pasar! react usa className no class

--- src/resources/js/Components/Timer.jsx
+++ src/resources/js/Components/Timer.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Timer(props) {
   return (
-    <div class="">
-      <span class="">
+    <div className="">
+      <span className="">
         {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
       </span>
-      <span class="">
+      <span className="">
         {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
       </span>
-      <span class="">
+      <span className="">
         {("0" + ((props.time / 10) % 100)).slice(-2)}
       </span>
     </div>
