20547ea295d47fb13fe8fa627bfc78e5b1db23a2
Ignacio Núñez <inr_jw@hotmail.com>
2022-09-09 13:33:35 +0200

    Ahora vite cargará imagenes

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -5,17 +5,22 @@ import '../css/app.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
-//import { InertiaProgress } from '@inertiajs/progress';
+import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(
+        `./Pages/${name}.jsx`,
+        import.meta.glob([
+            './Pages/**/*.jsx',
+            '../images/**'
+        ])),
     setup({ el, App, props }) {
         return render(<App {...props} />, el);
     },
 });
 
-//InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563' });
